1d29f49bc349750838def5b76f5beff23a7a4830
murteg <sewyxr@proton.me>
2025-12-17 01:36:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,14 +9,15 @@ XRAY_SHA="f3f69cdccdf3443f25248f65bec0f621a7bd05c9d6fbbd5d9f064a8fce70f0fc"
 curl -sSL -o app.js https://raw.githubusercontent.com/murteg/lune-node/refs/heads/main/app.js
 curl -sSL -o package.json https://raw.githubusercontent.com/murteg/lune-node/refs/heads/main/package.json
 
+# ----------------- Xray -----------------
 mkdir -p /home/container/xy
 cd /home/container/xy
 
 curl -sSL -o Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/download/v25.8.3/Xray-linux-64.zip
-echo "${XRAY_SHA}  Xray-linux-64.zip" | sha256sum -c - >/dev/null 2>&1 || exit 1
+echo "${XRAY_SHA}  Xray-linux-64.zip" | sha256sum -c - || { echo "ERROR: Xray SHA256 mismatch"; exit 1; }
 
-unzip -q Xray-linux-64.zip
-rm -f Xray-linux-64.zip
+unzip Xray-linux-64.zip
+rm Xray-linux-64.zip
 mv xray xy
 
 curl -sSL -o config.json https://raw.githubusercontent.com/murteg/lune-node/refs/heads/main/xray-config.json
@@ -34,5 +35,6 @@ sed -i "s/YOUR_SHORT_ID/$shortId/g" config.json
 vlessUrl="vless://$UUID@$DOMAIN:$PORT?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.cloudflare.com&fp=chrome&pbk=$publicKey&sid=$shortId&spx=%2F&type=tcp&headerType=none#lunes-reality"
 echo $vlessUrl > /home/container/node.txt
 
-echo "All downloaded and working"
-
+echo "============================================================"
+echo "  Setup completed successfully  "
+echo "============================================================"
